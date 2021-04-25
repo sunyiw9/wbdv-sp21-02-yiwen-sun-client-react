80969429b83cdf78f2b0c955cd0e4b79eda56c3d
sunyiw9 <syw9syw@gmail.com>
2021-04-25 01:31:12 -0700

    try to deploy a5

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,4 +1,4 @@
-const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+const QUIZZES_URL = 'http://localhost:3000/api/quizzes';
 const findAllQuizzes = () => {
     return fetch(QUIZZES_URL)
         .then(response => response.json())
@@ -7,6 +7,18 @@ const findQuizById = (qid) => {
     return fetch(`${QUIZZES_URL}/${qid}`)
         .then(response => response.json())
 }
+
+const submitQuiz = (quizId, questions) => {
+    return fetch(`${QUIZZES_URL}/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(result => console.log(result))
+}
+
 export default {
-    findAllQuizzes, findQuizById
+    findAllQuizzes, findQuizById, submitQuiz
 }
